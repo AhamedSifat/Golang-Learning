822b17ec31a3793a84631cf4457f895607a0d749
sifat1234 <ahahmedsifat111@gmail.com>
2025-11-07 02:23:40 +0600

    Hello World

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(1)
+	fmt.Println(2 + 2)
+	fmt.Println("Goodbye, World!")
+	fmt.Println(true)
+	fmt.Println(3.14 < 5)
+	fmt.Println("Sum:", 5 + 7)
+	fmt.Println("Is 10 greater than 5?", 10 > 5)
+	fmt.Println(false)
+	fmt.Println("End of program.")
+}
