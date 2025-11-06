e1179bca073968782d7fe9298d23740740debd1d
sifat1234 <ahahmedsifat111@gmail.com>
2025-11-07 02:44:41 +0600

    Variablee

---
+++ variables/variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/////
+	var greeting string = "Hello, World!"
+
+	fmt.Println(greeting)
+
+	var number int = 1
+	fmt.Println(number)
+
+	var sum int
+	sum = 2 + 2
+	fmt.Println(sum)
+
+	// This is a comment
+	var name = "Sifat"
+	fmt.Println(name)
+
+	var isActive bool = true
+	fmt.Println(isActive)
+
+	// This is a comment
+	compare := 3.14 < 5
+	fmt.Println(compare)
+}
