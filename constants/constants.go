09f3bead314ba39400df560629b44b70d7914c64
sifat1234 <ahahmedsifat111@gmail.com>
2025-11-07 14:45:09 +0600

    Constants

---
+++ constants/constants.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+
+	const (
+		age  = 39
+		name = "Sifat"
+	)
+
+    fmt.Println("Value of Pi:", Pi)
+		fmt.Println("Name:", name)
+		fmt.Println("Age:", age)
+}
